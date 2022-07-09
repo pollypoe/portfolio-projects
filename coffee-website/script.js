46eb0bb0b8ab03a555844bd9c9207fdaf3bd3693
pollypoe <59973445+pollypoe@users.noreply.github.com>
2022-07-09 13:55:57 -0400

    Add files via upload

---
+++ coffee-website/script.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener("click", () => {
+document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+  document.body.classList.remove("nav-open");
+  });
+  });
